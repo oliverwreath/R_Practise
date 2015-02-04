fc02d5c7a160462d5f1b6bb844d840eea6d8149a
oliverwreath <oliverwreath@users.noreply.github.com>
2015-02-04 16:18:06 +0800

    Create Summarizing a factor.R

---
+++ chapter-4-factors/Summarizing a factor.R
@@ -0,0 +1,10 @@
+survey_vector <- c("M","F","F","M","M")
+factor_survey_vector <- factor( survey_vector )
+levels(factor_survey_vector) <- c("Female","Male")
+factor_survey_vector
+
+# Type your code here for survey_vector
+summary(survey_vector)
+
+# Type your code here for factor_survey_vector
+summary(factor_survey_vector)
