5a573c6ee5650b97f6b703f2d811ad3ed1b0db50
oliverwreath <oliverwreath@users.noreply.github.com>
2015-02-04 15:27:12 +0800

    Create Adding a row.R

---
+++ chapter-3-matrices/Adding a row.R
@@ -0,0 +1,10 @@
+# Box office Star Wars: In Millions (!) 
+
+# Matrix that contains the first trilogy box office
+star_wars_matrix  
+
+# Matrix that contains the second trilogy box office
+star_wars_matrix2 
+
+# Combine both Star Wars trilogies in one matrix
+all_wars_matrix <- rbind(star_wars_matrix, star_wars_matrix2)
