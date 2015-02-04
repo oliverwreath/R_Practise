3b7f7e1dce29ee1d33d0611283805d3481bd2f4d
oliverwreath <oliverwreath@users.noreply.github.com>
2015-02-04 15:11:12 +0800

    Create Analyzing matrices, you shall.R

---
+++ chapter-3-matrices/Analyzing matrices, you shall.R
@@ -0,0 +1,8 @@
+# Box office Star Wars: In Millions!
+# The first element: US, the second element: Non-US 
+new_hope <- c(460.998007, 314.4)
+empire_strikes <- c(290.475067, 247.900000)
+return_jedi <- c(309.306177,165.8)
+
+# Add your code below to construct the matrix
+star_wars_matrix <- matrix(c(new_hope, empire_strikes, return_jedi), byrow=T, nrow=3)
