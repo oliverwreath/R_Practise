2808725fcef177842783f7e0e121ca9d7212bde0
oliverwreath <oliverwreath@users.noreply.github.com>
2015-02-04 17:10:46 +0800

    Create Selection of data frame elements.R

---
+++ chapter-5-data-frames/Selection of data frame elements.R
@@ -0,0 +1,8 @@
+# The planets_df data frame from the previous exercise is pre-loaded
+closest_planets_df <- planets_df[1:3,]
+
+furthest_planets_df <- planets_df[6:8,]
+
+# Have a look:
+closest_planets_df
+furthest_planets_df
