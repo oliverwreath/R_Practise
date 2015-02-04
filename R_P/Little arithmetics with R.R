cc05fef0bccfaea819f42c26cd4d85e0ef8cc7a0
oliverwreath <oliverwreath@users.noreply.github.com>
2015-02-04 11:14:00 +0800

    Create Little arithmetics with R.R

---
+++ R_P/Little arithmetics with R.R
@@ -0,0 +1,19 @@
+# An addition
+5+5 
+
+# A subtraction
+5-5 
+
+# A multiplication
+3*5
+
+ # A division
+(5+5)/2 
+
+# Exponentiation
+# Add your code here!
+2^5
+
+# Modulo
+# Add your code here!
+17%%4
