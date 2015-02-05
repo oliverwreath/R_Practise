9d665ab0038d031c03a577e10a72712e1772d052
oliverwreath <oliverwreath@users.noreply.github.com>
2015-02-05 09:22:53 +0800

    Create Only planets with rings (2).R

---
+++ chapter-5-data-frames/Only planets with rings (2).R
@@ -0,0 +1,4 @@
+# Note that the planets_df dataset and rings_vector are pre-loaded in your workspace
+
+# Select the information on planets with rings:
+planets_with_rings_df <- planets_df[rings_vector,]
