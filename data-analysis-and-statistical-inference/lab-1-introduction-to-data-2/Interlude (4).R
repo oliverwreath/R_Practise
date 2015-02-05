51e16cb27224582e8a26fc76532ad7e3df492da7
oliverwreath <oliverwreath@users.noreply.github.com>
2015-02-05 12:26:29 +0800

    Create Interlude (4).R

---
+++ data-analysis-and-statistical-inference/lab-1-introduction-to-data-2/Interlude (4).R
@@ -0,0 +1,9 @@
+# The cdc data frame is already loaded into the workspace
+
+# Create the subsets:
+resp1000_smk = cdc[1000, 'smoke100']
+first30_ht = cdc[1:30, 'height']
+
+# Print the subsets:
+resp1000_smk
+first30_ht
