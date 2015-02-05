d1d56f3875ec0c0c7299da18c61c3eb8ee94d368
oliverwreath <oliverwreath@users.noreply.github.com>
2015-02-05 10:33:08 +0800

    Create Load a data set into R.R

---
+++ data-analysis-and-statistical-inference/Load a data set into R.R
@@ -0,0 +1,5 @@
+# Read in your data set and assign to the object:
+present  = read.table('http://s3.amazonaws.com/assets.datacamp.com/course/dasi/present.txt')
+
+# This will print your data set in the console:
+present
