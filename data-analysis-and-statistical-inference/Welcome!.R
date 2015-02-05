d0a90549150851591a01cad0ab07df3ec3d10dc3
oliverwreath <oliverwreath@users.noreply.github.com>
2015-02-05 10:30:06 +0800

    Create Welcome!.R

---
+++ data-analysis-and-statistical-inference/Welcome!.R
@@ -0,0 +1,15 @@
+# This is the editor and the part below the white line is called the console.
+
+# The hashtag is used to add comments
+
+# An addition
+5 + 5
+
+# A substraction
+5 - 5
+
+# A division
+(5 + 5)/2
+
+# A plot
+plot(cars, type="o", col="blue")
