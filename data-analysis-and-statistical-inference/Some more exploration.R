cd1c65d7576d247577bfa3bcee84bb8eeba04424
oliverwreath <oliverwreath@users.noreply.github.com>
2015-02-05 10:46:13 +0800

    Create Some more exploration.R

---
+++ data-analysis-and-statistical-inference/Some more exploration.R
@@ -0,0 +1,10 @@
+# The present dataset is already loaded.
+# Find the number of boys, and assign your answer to 
+boys = present$boys
+
+# Find the number of girls, and assign your answer to
+girls = present$girls
+
+# This will print the results
+boys
+girls
