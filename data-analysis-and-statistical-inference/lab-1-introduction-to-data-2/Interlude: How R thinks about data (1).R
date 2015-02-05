1d3af3b49eb816818cf4201ecb5c80df62a5723a
oliverwreath <oliverwreath@users.noreply.github.com>
2015-02-05 12:18:17 +0800

    Create Interlude: How R thinks about data (1).R

---
+++ data-analysis-and-statistical-inference/lab-1-introduction-to-data-2/Interlude: How R thinks about data (1).R
@@ -0,0 +1,10 @@
+# The cdc data frame is already loaded into the workspace
+
+# Create the subsets:
+height_1337 = cdc[1337, 'height']
+weight_111 = cdc[111, 'weight']
+
+# Print the results:
+height_1337
+weight_111
+
