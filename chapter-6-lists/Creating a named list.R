c1b317f67f9b18b6af85ed8e632dbd3dbf017691
oliverwreath <oliverwreath@users.noreply.github.com>
2015-02-05 09:57:10 +0800

    Create Creating a named list.R

---
+++ chapter-6-lists/Creating a named list.R
@@ -0,0 +1,9 @@
+# Vector with numerics from 1 up to 10
+my_vector <- 1:10 
+# Matrix with numerics from 1 up to 9
+my_matrix <- matrix(1:9,ncol=3)
+# First 10 elements of the built-in data frame mtcars
+my_df     <- mtcars[1:10,] 
+
+# Construct list with these different elements:
+my_list <- list(VECTOR=my_vector,MATRIX=my_matrix,DATAFRAME=my_df)
