b5dd73c598371589851ea79ae612d006dcf7aadf
oliverwreath <oliverwreath@users.noreply.github.com>
2015-02-05 12:20:53 +0800

    Create Interlude (2).R

---
+++ data-analysis-and-statistical-inference/lab-1-introduction-to-data-2/Interlude (2).R
@@ -0,0 +1,10 @@
+# The cdc data frame is already loaded into the workspace
+
+# Create the subsets:
+first8 = cdc[1:8, 3:5]
+wt_gen_10_20 = cdc[10:20, 6:9]
+
+# Print the subsets:
+first8
+wt_gen_10_20
+
