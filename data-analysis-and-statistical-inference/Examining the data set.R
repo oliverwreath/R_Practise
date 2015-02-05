c3bae1961bc1b0b13657846b719c3c5b02e5abdb
oliverwreath <oliverwreath@users.noreply.github.com>
2015-02-05 10:34:15 +0800

    Create Examining the data set.R

---
+++ data-analysis-and-statistical-inference/Examining the data set.R
@@ -0,0 +1,6 @@
+# The present data frame is already loaded.
+# Print the number of rows and variables with the 'dim' function:
+dim(present)
+
+# Print the names of the variables of the data frame:
+names(present)
