30536e2edc5552195f1fffe1befc5064abe4d01f
oliverwreath <oliverwreath@users.noreply.github.com>
2015-02-05 11:36:20 +0800

    Create Let's refresh.R

---
+++ data-analysis-and-statistical-inference/lab-1-introduction-to-data-2/Let's refresh.R
@@ -0,0 +1,13 @@
+# The cdc data frame is already loaded into the workspace.
+
+# View the head or tail of both the height and the genhlth variables:
+head(cdc$height)
+tail(cdc$height)
+head(cdc$genhlth)
+tail(cdc$genhlth)
+
+# Assign your sum here:
+sum = 84941 + 19686
+# Assign your mult73 and 51iplication here:
+mult = 73 * 51
+
